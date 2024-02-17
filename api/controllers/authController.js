43a526888cddeb40d4b011d7223990e0edc352ee
Nurain <nurainbamidelejnr@gmail.com>
2024-02-17 23:57:00 +0100

    error handler

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,16 +1,23 @@
 const bcryptjs = require("bcryptjs")
-const User = require("../models/userModel")
+const User = require("../models/userModel");
+const errorHandler  = require("../utils/error");
 
-const signup = async (req, res) => {
-    const {username, email, password} = req.body
+const signup = async (req, res, next) => {
+    const { username, email, password } = req.body
+
+    // Check if password meets minimum length requirement
+    if (password.length < 6) {
+        return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+
     const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = new User({username, email, password : hashedPassword})
+    const newUser = new User({ username, email, password: hashedPassword })
 
     try {
         await newUser.save()
-    res.status(201).json({message: "User created succesfully"})
+        res.status(201).json({ message: "User created succesfully" })
     } catch (error) {
-     res.status(500).json(error.message)   
+        next(error)
     }
 }
 
