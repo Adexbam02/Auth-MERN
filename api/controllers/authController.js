f999c0d3b3140cf35d9a41e61ce1e055b37015ce
Nurain <nurainbamidelejnr@gmail.com>
2024-02-17 20:55:16 +0100

    bycrypt initiated

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,11 +1,17 @@
+const bcryptjs = require("bcryptjs")
 const User = require("../models/userModel")
 
 const signup = async (req, res) => {
     const {username, email, password} = req.body
-    const newUser = new User({username, email, password})
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({username, email, password : hashedPassword})
 
-    await newUser.save()
+    try {
+        await newUser.save()
     res.status(201).json({message: "User created succesfully"})
+    } catch (error) {
+     res.status(500).json(error.message)   
+    }
 }
 
 module.exports = signup
