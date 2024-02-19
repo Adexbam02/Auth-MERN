5725ab32d87779e6dc5a878400157171501d0521
Nurain <nurainbamidelejnr@gmail.com>
2024-02-19 04:13:24 +0100

    Sign up page UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,7 @@
+// import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  return <div className="p-3 max-w-lg mx-auto">Sign in</div>;
+};
 
-export default SignIn
+export default SignIn;
