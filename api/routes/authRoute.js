62b640d03d90eb840c5a69ec18e4d4119d5e66c0
Nurain <nurainbamidelejnr@gmail.com>
2024-02-22 00:44:16 +0100

    sign in api

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const signup = require('../controllers/authController.js')
+const signin = require('../controllers/authController.js')
 const router = express.Router()
 
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 module.exports = router
